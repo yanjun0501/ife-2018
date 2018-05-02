e22fa507b959ffe8ad0f1f8f015235458b2faef0
LiYanjun02 <yanjun.li02@ele.me>
2018-05-03 00:32:53 +0800

    design 6

--- design-6/js/index.js
+++ design-6/js/index.js
@@ -1,11 +1,19 @@
 var control = document.getElementsByClassName('control')
 
 control[0].addEventListener('click', function(e) {
-    console.log(e.target)
+    // console.log(e.target)
     var list = document.getElementsByClassName('radio')
     list = Array.prototype.slice.call(list)
     list.forEach(function(item) {
+        item.classList.remove('checked')
+    });
+    var wheel = document.getElementsByClassName('wheel')
+    wheel = Array.prototype.slice.call(wheel)
+    wheel.forEach(function(item) {
         item.classList.remove('clicked')
     });
-    e.target.classList.add('clicked')
+    e.target.classList.add('checked')
+    // document.getElementById(e.)
+    document.getElementsByClassName('wheel')[e.target.id - 1].classList.add('clicked')
+    // wheel = Array.prototype.slice.call(wheel)
 })
