d6b009f83b9200d4743e54d0a4b94eb394eb6d55
LiYanjun02 <yanjun.li02@ele.me>
2018-05-04 15:54:42 +0800

    update

--- mvvm-2.7.2/src/index.js
+++ mvvm-2.7.2/src/index.js
@@ -5,18 +5,10 @@ import 'babel-polyfill';
 require('./index.css');
 
 // route
-import GrFather from './components/GrFather/GrFather.san';
-import HelloWorld from './components/HelloWorld/HelloWorld.san';
-import Loop from './components/Loop/Loop.san';
-import Block from './components/Block/Block.san';
-import DataForm from './components/DataForm/DataForm.san';
+import App from './App.san';
 import { router } from 'san-router';
 
-router.add({rule: '/', Component: GrFather, target: '#app'});
-router.add({rule: '/block', Component: Block, target: '#app'});
-router.add({rule: '/loop', Component: Loop, target: '#app'});
-router.add({rule: '/dataform', Component: DataForm, target: '#app'});
-router.add({rule: '/helloworld', Component: HelloWorld, target: '#app'});
+router.add({rule: '/', Component: App, target: '#app'});
 
 // start
 router.start();
